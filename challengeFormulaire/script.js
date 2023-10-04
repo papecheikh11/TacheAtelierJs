a92e1b8c60793543c13e7b2b6b3cd7ec0f3db2ec
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-10-04 13:06:17 +0000

    modifier challenge atelier

--- challengeFormulaire/script.js
+++ challengeFormulaire/script.js
@@ -1,50 +1,24 @@
-let utilisateur = document.getElementById("utilisateur");
-
-function addUser() {
-  let prenom = document.getElementById("prenom");
-  let nom = document.getElementById("nom");
-  let email = document.getElementById("mail");
-  let telephone = document.getElementById("telephone");
-
-  let utilisateurPrenom = prenom.value;
-  let utilisateurNom = nom.value;
-  let utilisateurEmail = email.value;
-  let utilisateurTelephone = telephone.value;
-
-  if (
-    utilisateurPrenom === "" &&
-    utilisateurNom === "" &&
-    utilisateurEmail === "" &&
-    utilisateurTelephone === ""
-  ) {
-    return;
-  }
-
-  let trPrenom = createElement("tr");
-  trPrenom.innerHTML = utilisateurPrenom;
-
-  let trNom = createElement("tr");
-  trNom.innerHTML = utilisateurPrenom;
-
-  let trEmail = createElement("tr");
-  trEmail.innerHTML = utilisateurPrenom;
-
-  let trTelephone = createElement("tr");
-  trTelephone.innerHTML = utilisateurPrenom;
-
-  let editButton = document.createElement("button");
-  editButton.innerText = "Modifier";
-
-  editButton.onclick = function () {
-    editUser(tr)
-  }
-
-  let suppButton = document.createElement('button')
-    suppButton.innerText = 'Supprimer'
-
-    suppButton.onclick = function(){
-        suppUser(tr)
-    }
+let prenom = document.getElementById("prenom");
+let nom = document.getElementById("nom");
+let email = document.getElementById("mail");
+let telephone = document.getElementById("telephone");
+let ajouter = document.getElementById("ajouter");
 
 
-}
+
+let users = document.getElementById("users");
+let utilisateurs = [];
+
+ajouter.addEventListener("click", function () {
+  let utilisateur = {
+    prenom: prenom.value,
+    nom: nom.value,
+    email: email.value,
+    telephone: telephone.value,
+  };
+
+  utilisateurs.push(utilisateur);
+  console.log(utilisateurs);
+
+  users.innerHTML += `<tr><td>${utilisateur.prenom}</td><td>${utilisateur.nom}</td><td>${utilisateur.email}</td><td>${utilisateur.telephone}</td><td><button>Modifier</button><button>Supprimer</button></td>`;
+});
