7ab531b669bd04198aec731466a8df6944cee6a9
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-10-04 22:24:37 +0000

    modifier challenge des 30 jours

---
+++ 30_days_for_javascript/day_1_introduction/variable.js
@@ -0,0 +1,16 @@
+// Déclaration de différentes variables de différents types de données
+
+let firstName = 'Asabeneh'    // prenom de la personne
+let lastName = 'Yetayeh'      // nom de la personne
+let country = 'Finland'       // pays
+let city = 'Helsinki'         // capital ville
+let age = 100                 // age dans l'année
+let isMarried = true
+
+// Declaration des variables avec des valeurs
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
--- challengeFormulaire/script.js
+++ challengeFormulaire/script.js
@@ -4,7 +4,7 @@ let email = document.getElementById("mail");
 let telephone = document.getElementById("telephone");
 let ajouter = document.getElementById("ajouter");
 
-let modifier = document.getElementById('modifier')
+let modifier = document.getElementById("modifier");
 
 let users = document.getElementById("users");
 let utilisateurs = [];
@@ -18,19 +18,22 @@ ajouter.addEventListener("click", function () {
   };
 
   utilisateurs.push(utilisateur);
-  prenom.value =''
-  nom.value=''
-  email.value=''
-  telephone.value=''
+  prenom.value = "";
+  nom.value = "";
+  email.value = "";
+  telephone.value = "";
   console.log(utilisateurs);
+  users.innerHTML = "";
+  utilisateurs.forEach((element) => {
+    users.innerHTML += `<tr><td>${element.prenom}</td><td>${element.nom}</td><td>${element.email}</td><td>${element.telephone}</td><td><button onclick ="editer()">Modifier</button><button onclick = "supprimer()">Supprimer</button></td></tr>`;
 
-  users.innerHTML += `<tr><td>${utilisateur.prenom}</td><td>${utilisateur.nom}</td><td>${utilisateur.email}</td><td>${utilisateur.telephone}</td><td><button onclick ="editer()">Modifier</button><button onclick = "supprimer()">Supprimer</button></td></tr>`;
+  });
 });
 
 function supprimer() {
-    users.innerHTML =''
+  //   users.innerHTML = "";
 }
-function editer() {
-   modifier.style.display ="block"
-   ajouter.style.display ="none"
+function editer(index) {
+    modifier.style.display = "block"
+    ajouter.style.display = "none"
 }
