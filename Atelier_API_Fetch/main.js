11f09dc6a3dea05ded1ec69f2b0c6039ae804d09
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-10-20 01:50:36 +0000

    modifier API

--- Atelier_API_Fetch/main.js
+++ Atelier_API_Fetch/main.js
@@ -3,12 +3,19 @@ async function getTodo() {
     const response = await fetch("https://jsonplaceholder.typicode.com/todos");
     const data = await response.json();
     let todoList = document.getElementById("todoList");
-    const todoListFini = document.getElementById("todoListFini");
+
+    // Pour afficher et cacher la notification
+    let todoListFini = document.getElementById("todoListFini");
+    const btnNotif = document.querySelector(".notif");
+    btnNotif.addEventListener("click", () => {
+      todoListFini.classList.toggle("isVisible");
+    });
+    let incremente = 0;
 
     data.forEach((todo) => {
       const listItem = document.createElement("li");
       listItem.textContent = todo.title;
-      console.log(todo.title);
+      // console.log(todo.title);
 
       const btnFini = document.createElement("button");
       btnFini.textContent = "Terminer";
@@ -16,16 +23,25 @@ async function getTodo() {
       btnFini.addEventListener("click", () => {
         listItem.remove();
         todoList.insertBefore(listItem, todoList.firstChild);
+        //Incrementation du boutton
+        if (btnFini.textContent == "Terminer") {
+          incremente++;
+        btnNotif.dataset.counter = incremente;
+        }
+        else if(btnFini.textContent == "Non Terminer"){
+          incremente--;
+          btnNotif.dataset.counter = incremente;
+        }
+        // console.log(incremente);
+
         // Déplacer le todo vers les taches terminés
         todoListFini.appendChild(listItem);
         btnFini.textContent = "Non Terminer";
-
 
         btnFini.addEventListener("click", () => {
           listItem.remove();
           todoList.appendChild(listItem);
           btnFini.textContent = "Terminer";
-
         });
       });
       listItem.appendChild(btnFini);
