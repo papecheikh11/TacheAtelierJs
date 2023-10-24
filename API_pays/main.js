fbfcb415247ffc4109d7434fccf384f06169c0b1
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-10-24 18:55:53 +0000

    modifier API_Countries

--- API_pays/main.js
+++ API_pays/main.js
@@ -1,30 +1,58 @@
-const countriesContainer = document.querySelector('.countries-container')
-
-fetch("https://restcountries.com/v3.1/all/")
-  .then((res) => res.json())
-  .then((data) => {
-    data.forEach((country) => {
-        console.log(country.population);
-        const countryCard = document.createElement('a')
-        countryCard.classList.add('country-card')
-        countryCard.href = `country.html?name=${country.name.common}`
+const countriesContainer = document.querySelector(".countries-container");
+const myFetch = ('https://restcountries.com/v3.1/all/')
+async function mesPays(api){
+    try {
+        const btnSearch = document.querySelector(".fa-solid");
+        fetch(api)
+        .then((res) => res.json())
+        .then((data) => {
+        data.forEach((pays) => {
+        const countryCard = document.createElement("a");
+        countryCard.classList.add("country-card");
+        countryCard.href = `country.html?name=${pays.name.common}`;
         countryCard.innerHTML = `
-        <img src="${country.flags.svg}" alt="flag">
-        <div class="card-text">
-            <h3 class="card-title">${country.name.common}</h3>
-            <p><b>Population: </b>${country.population.toLocaleString('en-IN')}</p>
-            <p><b>Region: </b>${country.region}</p>
-            <p><b>Capital: </b>${country.capital?.[0]}</p>
-        </div>
-        `
+            <img src="${pays.flags.svg}" alt="flag">
+            <div class="card-text">
+                <h3 class="card-title">${pays.name.common}</h3>
+                <p><b>Population: </b>${pays.population.toLocaleString("en-US")}</p>
+                <p><b>Region: </b>${pays.region}</p>
+                <p><b>Capital: </b>${pays.capital?.[0]}</p>
+            </div>
+            `;
 
-        countriesContainer.append(countryCard)
+      countriesContainer.append(countryCard);
     });
   });
 
-  const darkMode = document.querySelector('.fa-moon')
-  const body = document.querySelector('body')
-  darkMode.addEventListener('click', () =>{
-    body.classList.toggle('maMode')
-    console.log(body);
-  })
+const darkMode = document.querySelector(".fa-moon");
+const body = document.querySelector("body");
+darkMode.addEventListener("click", () => {
+  body.classList.toggle("maMode");
+  btnSearch.classList.toggle(".fa-solidMode");
+});
+
+console.log(pays);
+
+    } catch (error) {
+        
+        console.log(error);
+    }
+}
+mesPays(myFetch)
+
+const paysAll = document.querySelector(".mesElements");
+const region = document.querySelector(".mesElements2")
+
+paysAll.addEventListener("input", (e) => {
+    const valSaisie = e.target.value
+    console.log(valSaisie);
+    countriesContainer.innerHTML = ''
+mesPays(`https://restcountries.com/v3.1/name/${valSaisie}`)
+})
+
+region.addEventListener('input', (e) => {
+    const valSaisie = e.target.value
+    console.log(valSaisie);
+    countriesContainer.innerHTML = ''
+mesPays(`https://restcountries.com/v3.1/region/${valSaisie}`)
+})
